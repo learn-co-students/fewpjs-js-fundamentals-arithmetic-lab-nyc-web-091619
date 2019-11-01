80db9c4840efa1ce7d935124f6bb95d0fababf09
H. Turcan Cicek <htcicek@users.noreply.github.com>
2019-11-01 10:58:10 -0400

    met all specs

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
+// Write your code here
 
 // Write your code here
+const num1 = 62;
+const num2 = 1;
+const multiply = num1 * num2;
+const random = Math.ceil(Math.random() * 10);
+const mod = 10 % 6;
+const max = Math.max(1, 20);
